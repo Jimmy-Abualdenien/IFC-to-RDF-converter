7f96acf6c45f2adb13cafa7fc088c61c67911064
jyrkio <Jyrki.Oraskari@aalto.fi>
2014-09-02 18:18:34 +0300

    The code was changed under MIT license (https://tldrlegal.com/license/mit-license).

--- converter/src/fi/ni/ifc2x3/IfcElement.java
+++ converter/src/fi/ni/ifc2x3/IfcElement.java
@@ -5,9 +5,27 @@ import java.util.*;
 
 /*
  * IFC Java class
- * @author Jyrki Oraskari
- * @license This work is licensed under a Creative Commons Attribution 3.0 Unported License.
- * http://creativecommons.org/licenses/by/3.0/ 
+The MIT License (MIT)
+
+Copyright (c) 2014 Jyrki Oraskari
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 
 public class IfcElement extends IfcProduct implements IfcStructuralActivityAssignmentSelect
@@ -16,11 +34,11 @@ public class IfcElement extends IfcProduct implements IfcStructuralActivityAssig
  String tag;
  // The inverse attributes
 
- InverseLinksList<IfcRelConnectsStructuralElement> hasStructuralMember= new InverseLinksList<IfcRelConnectsStructuralElement>();
  InverseLinksList<IfcRelFillsElement> fillsVoids= new InverseLinksList<IfcRelFillsElement>();
  InverseLinksList<IfcRelConnectsElements> connectedTo= new InverseLinksList<IfcRelConnectsElements>();
  InverseLinksList<IfcRelCoversBldgElements> hasCoverings= new InverseLinksList<IfcRelCoversBldgElements>();
  InverseLinksList<IfcRelProjectsElement> hasProjections= new InverseLinksList<IfcRelProjectsElement>();
+ InverseLinksList<IfcRelConnectsStructuralElement> hasStructuralMember= new InverseLinksList<IfcRelConnectsStructuralElement>();
  InverseLinksList<IfcRelReferencedInSpatialStructure> referencedInStructures= new InverseLinksList<IfcRelReferencedInSpatialStructure>();
  InverseLinksList<IfcRelConnectsPortToElement> hasPorts= new InverseLinksList<IfcRelConnectsPortToElement>();
  InverseLinksList<IfcRelVoidsElement> hasOpenings= new InverseLinksList<IfcRelVoidsElement>();
@@ -41,15 +59,6 @@ public class IfcElement extends IfcProduct implements IfcStructuralActivityAssig
  }
 
  // Getters and setters of inverse values
-
- public InverseLinksList<IfcRelConnectsStructuralElement> getHasStructuralMember() {
-   return hasStructuralMember;
-
- }
- public void setHasStructuralMember(IfcRelConnectsStructuralElement value){
-   this.hasStructuralMember.add(value);
-
- }
 
  public InverseLinksList<IfcRelFillsElement> getFillsVoids() {
    return fillsVoids;
@@ -84,6 +93,15 @@ public class IfcElement extends IfcProduct implements IfcStructuralActivityAssig
  }
  public void setHasProjections(IfcRelProjectsElement value){
    this.hasProjections.add(value);
+
+ }
+
+ public InverseLinksList<IfcRelConnectsStructuralElement> getHasStructuralMember() {
+   return hasStructuralMember;
+
+ }
+ public void setHasStructuralMember(IfcRelConnectsStructuralElement value){
+   this.hasStructuralMember.add(value);
 
  }
 
