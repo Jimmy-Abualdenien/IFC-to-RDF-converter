6c35a78bb499302516f39f73c120747845ffa8dd
Pieter Pauwels <pipauwel.pauwels@ugent.be>
2014-10-15 11:52:23 +0200

    removed unused namespaces

--- converter/src/fi/ni/rdf/Namespace.java
+++ converter/src/fi/ni/rdf/Namespace.java
@@ -6,10 +6,7 @@ public class Namespace {
 	public static final String RDFS = "http://www.w3.org/2000/01/rdf-schema#";
 	public static final String OWL = "http://www.w3.org/2002/07/owl#";
 	public static final String XSD = "http://www.w3.org/2001/XMLSchema#";
-	public static final String LIST = "http://www.w3.org/2000/10/swap/list#";
-	public static final String DCE = "http://purl.org/dc/elements/1.1/";
-	public static final String DCT = "http://purl.org/dc/terms/";	
-	
 	public static String IFC = "http://buildingsmart.org/ontology/ifc2x3#";
-	public static String OLO = "http://purl.org/ontology/olo/core#";
+//	public static final String DCE = "http://purl.org/dc/elements/1.1/";
+//	public static final String DCT = "http://purl.org/dc/terms/";	
 }
