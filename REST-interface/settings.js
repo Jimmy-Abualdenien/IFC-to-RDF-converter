baab06401564ce5bb57db4c0cc90ebc62c7b64ba
Pieter Pauwels <pipauwel.pauwels@ugent.be>
2015-02-17 22:38:01 +0100

    removed comments from REST-interface docs

--- REST-interface/settings.js
+++ REST-interface/settings.js
@@ -2,6 +2,4 @@ settings = {
 	rdf_host: 'http://localhost',
 	server_port: 3333,
 	base_path: "./repo/",
-	cwm_command: {"command": "C:/python24/python.exe", "args": ["-W", "ignore::DeprecationWarning", "C:/Program Files/cwm-1.2.1/swap/cwm.py"]}
-	//cwm_command: {"command": "cwm", "args": []}//'http://ifc.mmlab.be',
 };
